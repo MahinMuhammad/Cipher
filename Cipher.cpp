b944d59eaa34eda1ea9357bbf0e9e04b7d3bd834
MahinMuhammad <amimahinurrahman@gmail.com>
2022-06-03 19:48:38 +0600

    using unicode instead of selfmade functions

--- Cipher.cpp
+++ Cipher.cpp
@@ -3,101 +3,6 @@
 #include<string.h>
 using namespace std;
 
-static char c; // keeping the input out case same
-
-int position(char word)
-{
-    c = word;
-    word = tolower(word);
-    int po;
-    switch (word)
-    {
-    case 'a':
-        po = 0;
-        break;
-    case 'b':
-        po = 1;
-        break;
-    case 'c':
-        po = 2;
-        break;
-    case 'd':
-        po = 3;
-        break;
-    case 'e':
-        po = 4;
-        break;
-    case 'f':
-        po = 5;
-        break;
-    case 'g':
-        po = 6;
-        break;
-    case 'h':
-        po = 7;
-        break;
-    case 'i':
-        po = 8;
-        break;
-    case 'j':
-        po = 9;
-        break;
-    case 'k':
-        po = 10;
-        break;
-    case 'l':
-        po = 11;
-        break;
-    case 'm':
-        po = 12;
-        break;
-    case 'n':
-        po = 13;
-        break;
-    case 'o':
-        po = 14;
-        break;
-    case 'p':
-        po = 15;
-        break;
-    case 'q':
-        po = 16;
-        break;
-    case 'r':
-        po = 17;
-        break;
-    case 's':
-        po = 18;
-        break;
-    case 't':
-        po = 19;
-        break;
-    case 'u':
-        po = 20;
-        break;
-    case 'v':
-        po = 21;
-        break;
-    case 'w':
-        po = 22;
-        break;
-    case 'x':
-        po = 23;
-        break;
-    case 'y':
-        po = 24;
-        break;
-    case 'z':
-        po = 25;
-        break;
-
-    default:
-        break;
-    }
-return po;
-
-}
-
 int newPos(int a) //a is position
 {
     int r;
@@ -118,133 +23,6 @@ int newPos(int a) //a is position
     r = a % 26;
     return r;
 }
-
-char newWord(int np) // np is new position
-{
-    char nw; // nw is new word
-    switch (np)
-    {
-    case 0:
-        nw = 'A';
-        break;
-    case 1:
-        nw = 'B';
-        break;
-    case 2:
-        nw = 'C';
-        break;
-    case 3:
-        nw = 'D';
-        break;
-    case 4:
-        nw = 'E';
-        break;
-    case 5:
-        nw = 'F';
-        break;
-    case 6:
-        nw = 'G';
-        break;
-    case 7:
-        nw = 'H';
-        break;
-    case 8:
-        nw = 'I';
-        break;
-    case 9:
-        nw = 'J';
-        break;
-    case 10:
-        nw = 'K';
-        break;
-    case 11:
-        nw = 'L';
-        break;
-    case 12:
-        nw = 'M';
-        break;
-    case 13:
-        nw = 'N';
-        break;
-    case 14:
-        nw = 'O';
-        break;
-    case 15:
-        nw = 'P';
-        break;
-    case 16:
-        nw = 'Q';
-        break;
-    case 17:
-        nw = 'R';
-        break;
-    case 18:
-        nw = 'S';
-        break;
-    case 19:
-        nw = 'T';
-        break;
-    case 20:
-        nw = 'U';
-        break;
-    case 21:
-        nw = 'V';
-        break;
-    case 22:
-        nw = 'W';
-        break;
-    case 23:
-        nw = 'X';
-        break;
-    case 24:
-        nw = 'Y';
-        break;
-    case 25:
-        nw = 'Z';
-        break;
-    
-    default:
-        break;
-    }
-
-switch (c)
-{
-    case 'a':
-    case 'b':
-    case 'c':
-    case 'd':
-    case 'e':
-    case 'f':
-    case 'g':
-    case 'h':
-    case 'i':
-    case 'j':
-    case 'k':
-    case 'l':
-    case 'm':
-    case 'n':
-    case 'o':
-    case 'p':
-    case 'q':
-    case 'r':
-    case 's':
-    case 't':
-    case 'u':
-    case 'v':
-    case 'w':
-    case 'x':
-    case 'y':
-    case 'z':
-        nw = tolower(nw); //to make sure the case of input and out put is same
-    break;
-
-default:
-    break;
-}
-
-return nw;
-}
-
 
 int main()
 {
@@ -283,8 +61,12 @@ int main()
                 {
                     for(int i=0; i<wrd.length(); i++)
                     {
-                        if((wrd.at(i)>='a' && wrd.at(i)<='z') || (wrd.at(i)>='A' && wrd.at(i)<='Z'))
-                            newwrd.insert(i, 1, newWord(newPos(position(wrd.at(i)) + diff)) );
+                        if(wrd.at(i)>='a' && wrd.at(i)<='z')
+                            newwrd.insert(i, 1, (newPos(((wrd.at(i))-97) + diff)+97) );
+
+                        else if(wrd.at(i)>='A' && wrd.at(i)<='Z')
+                            newwrd.insert(i, 1, (newPos(((wrd.at(i))-65) + diff)+65) );
+
                         else
                             newwrd.insert(i, 1, wrd.at(i));
                     }
@@ -293,8 +75,12 @@ int main()
                 {
                     for(int i=0; i<wrd.length(); i++)
                     {
-                        if((wrd.at(i)>='a' && wrd.at(i)<='z') || (wrd.at(i)>='A' && wrd.at(i)<='Z'))
-                            newwrd.insert(i, 1, newWord(newPos(position(wrd.at(i)) - diff)) );
+                        if(wrd.at(i)>='a' && wrd.at(i)<='z')
+                            newwrd.insert(i, 1, (newPos(((wrd.at(i))-97) - diff)+97) );
+
+                        else if(wrd.at(i)>='A' && wrd.at(i)<='Z')
+                            newwrd.insert(i, 1, (newPos(((wrd.at(i))-65) - diff)+65) );
+
                         else
                             newwrd.insert(i, 1, wrd.at(i));
                     }
